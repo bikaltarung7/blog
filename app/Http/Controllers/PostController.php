e575071997a1e41d74d8f10c6450b9ace099bd9e
bikaltarung7 <bikaltarung7@gmail.com>
2018-09-17 22:16:48 +0545

    Display posts, paginate

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,7 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+Use App\Post;
+
 class PostController extends Controller
 {
-    //
+    protected $limit = 3;
+
+    public function index()
+    {
+        $posts = Post::with('author')->latestFirst()->paginate($this->limit);
+        return view('blog.index',compact('posts'));
+    }
 }
--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,32 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+
+    public function author()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+
+    public function getImageUrlAttirbute($value)
+    {
+        $imageUrl = '';
+        if( ! is_null($this->image))
+        {
+            $imagepath = public_path() . "/img/" . $this->image;
+            if(file_exists($imagepath)) $imageUrl = asset("img/" . $this->image);
+        }
+
+        return $imageUrl;
+    }
+
+    public function getDateAttribute()
+    {
+        return $this->created_at->diffForHumans();
+    }
+
+    public function scopelatestFirst()
+    {
+        return $this->orderBy('created_at','desc');
+    }
 }
