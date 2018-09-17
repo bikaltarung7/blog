8b1c399831c241b54a413b4128433ba2e4c360bb
bikaltarung7 <bikaltarung7@gmail.com>
2018-09-17 22:15:44 +0545

    Added Users and Posts table seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        DB::table('users')->truncate(); 
+
+        DB::table('users')->insert([
+            'name' => 'Bikal',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('password')
+        ]);
+    }
+}
