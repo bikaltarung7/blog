84276889eecd3b93ba3e09125f39552bdd428ddb
bikaltarung7 <bikaltarung7@gmail.com>
2018-09-22 11:40:00 +0545

    Post factory

---
+++ database/factories/blogFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Post::class, function (Faker $faker) {
+    return [
+        'author_id' => 1,
+        'title' => $faker->sentence($nbWords = 5, $variableNbWords = true),
+        'slug' => $faker->slug, // secret
+        'excerpt' => $faker->paragraph($nbSentences = 2, $variableNbSentences = true),
+        'body' => $faker->text($maxNbChars = 200),
+    ];
+});
